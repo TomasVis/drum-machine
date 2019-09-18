28f3cc3cabfec4b9b1f79c58f9c7ee92d3c8818e
TomasVis <visokiams@gmail.com>
2019-09-18 22:02:16 +0300

    dev

--- src/App.js
+++ src/App.js
@@ -3,27 +3,62 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Sound from 'react-sound';
-import soundfile from './sound/rain.mp3'
+import soundfile from './sound/media.io_80s-Bdrum1.mp3'
+import soundfile1 from "./sound/media.io_80s-COWBELL1.mp3"
 
 
+class MyComponentWithSound extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      status: 'STOPPED',
+      file: soundfile
+    }
+    this.submitMessage = this.submitMessage.bind(this)
+  }
+
+  submitMessage(e){
+console.log("AAAAAAAAAAAAAA    "+  e);
+this.setState({
+  status: 'PLAYING'
+})
+}
+  render() {
+    return(<div>
+      <audio  src={soundfile1}>
+        Your browser does not support the HTML5 audio element.
+    </audio>
+     <Sound url={this.state.file}
+      playStatus={this.state.status}
+      playFromPosition={0 }
+      onLoading={this.handleSongLoading}
+      onPlaying={this.handleSongPlaying}
+      onFinishedPlaying={this.handleSongFinishedPlaying} />
+      <button onClick = {this.submitMessage} >Drum 1</button>
+      <button onClick = {this.submitMessage} >Drum 2</button>
+      <AudioPlayer/>
+      
+      </div>)
+  }
+}
 
 
 class App extends React.Component {
 
-
 render() {
-  return (
-    <Sound
-      url={soundfile}
-      playStatus={Sound.status.PLAYING}
-      playFromPosition={300 }
-      onLoading={this.handleSongLoading}
-      onPlaying={this.handleSongPlaying}
-      onFinishedPlaying={this.handleSongFinishedPlaying}
-    />
+  return (  
+    <MyComponentWithSound/>   
   );
 }
 }
-
+class AudioPlayer extends React.Component {
+  render() {
+    return (
+      <button>
+        <audio ref="audio_tag" src="./sound/media.io_80s-COWBELL1.mp3" />
+      </button>
+    );
+  }
+}
 
 export default App;
